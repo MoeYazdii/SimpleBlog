4fbdebdd45392204326c30ed1cc4f003a092df28
Moe <moeyazdii@proton.me>
2025-04-07 14:53:21 +0330

    Database change to postgresql and it connected

--- SimpleBlog/settings.py
+++ SimpleBlog/settings.py
@@ -73,12 +73,17 @@ WSGI_APPLICATION = 'SimpleBlog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SimpleBlogDB',
+        'USER': 'postgres',
+        'PASSWORD': 'qwertyuiop!',
+        'HOST': 'localhost'
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
